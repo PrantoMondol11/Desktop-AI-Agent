0cbf016322067750974612f1f78cfc601cc8db7b
PrantoMondol11 <mondolpranto83@gmail.com>
2025-07-29 12:31:56 -0500

    update

--- action.py
+++ action.py
@@ -1,8 +1,19 @@
 import text_to_speach
 import speachToText
+import webbrowser
 
-#user_data=speachToText.voice_to_text()
-#print(user_data)
-
-text_to_speach.text_to_speach("My name is lili,sir")
+user_data=speachToText.voice_to_text()
+print(user_data)
+if "what is your name" in user_data:
+    text_to_speach.text_to_speach("My name is lili,sir")
+elif "hello" in user_data:
+    text_to_speach.text_to_speach("hey sir")
+elif "shutdown" in user_data:
+    text_to_speach.text_to_speach("ok,sir")
+elif "play song" in  user_data:
+    text_to_speach.text_to_speach("playing song")
+    webbrowser.open("https://youtube.com")
+    
+else:
+    text_to_speach.text_to_speach("I am unable sir")
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+user_data=speachToText.voice_to_text()
+# print(user_data)
